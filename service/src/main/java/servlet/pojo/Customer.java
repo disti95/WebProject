d8d71311fc81543474d7ba153f311e360bea4d9d
disti95 <cheese412@hotmail.com>
2019-11-20 20:56:06 +0100

    merge master to servlets

--- service/src/main/java/servlet/pojo/Customer.java
+++ service/src/main/java/servlet/pojo/Customer.java
@@ -1,18 +1,28 @@
 package servlet.pojo;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
 /**
  * @author Christian Dittrich
  */
-@Getter
-@Setter
 public class Customer implements Serializable{
 
     private String email;
     private String password;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
