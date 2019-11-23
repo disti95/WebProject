1d588000a2c15738c3ad307586b564d3d1a42c41
disti95 <cheese412@hotmail.com>
2019-11-23 15:07:57 +0100

    cors test for frontend

--- service/src/main/java/rest/impl/db/postgres/test/PostgresTestImpl.java
+++ service/src/main/java/rest/impl/db/postgres/test/PostgresTestImpl.java
@@ -5,6 +5,7 @@ import rest.api.db.postgres.test.PostgresTestBean;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Christian Dittrich
@@ -16,8 +17,17 @@ public class PostgresTestImpl implements PostgresTest {
     private PostgresTestBean postgresTestBean;
 
     @Override
-    public String getPostgresTest() {
-        return postgresTestBean.getPostgresTest();
+    public Response getPostgresTest() {
+        return Response.ok(
+            String.format("{\"name\": \"%s\"}", postgresTestBean.getPostgresTest())
+        )
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Credentials", "true")
+        .header("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization")
+        .header("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        .build();
     }
 
     @Override
